5676fc98d370666d155d898ab7483a96b39dabbf
Parul2004 <parul1007.be21@chitkara.edu.in>
2022-10-01 23:05:25 -0700

    intial commit

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world\n";
+    cout<<"my name is parul sura \n";
+    cout<<"and my roll no is 2110991007\n";
+    std::cout<<"i am very brave girl\n ";
+    return 0;
+}
---
+++ variablesAndcomments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // this is single line comment
+
+    /*
+    this
+    is 
+    multiple line
+    comment.
+    */
+
+   cout<<"hello friends\n";
+    return 0;
+}
